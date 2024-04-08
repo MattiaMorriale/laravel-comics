7b2b6aeb3b908bda4d21f7023526ee7f1e918732
mattia_morriale <mattia.morriale1@gmail.com>
2024-04-08 17:22:36 +0200

    complete header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,29 @@
 <div class="nav">
-    <h1>header</h1>
+
+    <div class="nav-container">
+        <div class="logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+        </div>
+        <div class="list">
+            <ul class="m-0">
+                <li>CAHARACTERS</li>
+                <li  class="active">COMICS</li>
+                <li>MOVIE</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEO</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </div>
+    </div>
+
+</div>
+
+<div class="main">
+    <div class="jumbo">
+
+    </div>
 </div>
