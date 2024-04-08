cd974ba15e735cc55a2303592b74fe2d5a2d24ea
mattia_morriale <mattia.morriale1@gmail.com>
2024-04-08 18:00:02 +0200

    completed footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,81 @@
+<div class="footer">
+    <div class="jumbo">
+        <div class="box-img">
+            <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+        </div>
+        <div class="list">
+            <div>
+                <div>
+                    <ul class="m-0">
+                        <strong>DC COMICS</strong>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul class="m-0">
+                        <strong>SHOP</strong>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <ul class="m-0">
+                    <strong>DC</strong>
+                    <li>Terms Of Use</li>
+                    <li>Privacy Policy</li>
+                    <li>Ad Choise</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <div>
+                <ul class="m-0">
+                    <strong>SITES</strong>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="social">
+        <div class="btn">
+            <strong>SIGN-UP NOW</strong>
+        </div>
+        <div class="followUs">
+            <ul class="m-0">
+                <li><strong>FOLLOW US</strong></li>
+                <li>
+                    <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="">
+                </li>
+                <li>
+                    <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="">
+                </li>
+                <li>
+                    <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="">
+                </li>
+                <li>
+                    <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="">
+                </li>
+                <li>
+                    <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="">
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
